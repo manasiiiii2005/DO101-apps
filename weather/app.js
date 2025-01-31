a02027fdd823e0d84a91f8eb3b77e1a1da33e000
Manasi Dherange <manasi.dherange23@pcu.edu.in>
2025-01-31 10:16:06 +0530

    Update app.js

--- weather/app.js
+++ weather/app.js
@@ -23,7 +23,7 @@ app.use('/', indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+}); ;
 
 // error handler
 app.use(function(err, req, res, next) {
